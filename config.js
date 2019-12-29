7ae58f518e6684d5e8e373270e3be230aafa6f48
x4 <x4@x4s-mac-pro.home>
2019-12-28 23:57:01 -0500

    revision1

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {
-    style: 'mapbox://styles/data-maps/cjejfzobh4dlm2rqphkdjnm2o',
-    accessToken: 'pk.eyJ1IjoiZGF0YS1tYXBzIiwiYSI6ImNqZWpmeXhoajNtb2Eyd3FldG93OGpxejgifQ.xzOcuUd0LChdzHktllsAHw',
-    showMarkers: true,
+    style: 'mapbox://styles/vzpi/ck4q5subw0v2l1cmphq7aitcz',
+    accessToken: 'pk.eyJ1IjoidnpwaSIsImEiOiJjazRvaHViZ2kxcHdtM2pvZ21tdG90YTN3In0.i12GeLMjHHmBGpuTTSZsFQ',
+    showMarkers: false,
     theme: 'dark',
     alignment: 'left',
     title: 'The Danskammer Power Plant',
@@ -10,39 +10,68 @@ var config = {
     footer: 'Visualizing Pipeline Impacts | @pipelineimpacts',
     chapters: [
         {
-            id: 'slug-style-id',
-            title: 'Danskammer and Roseton Power Plants (left to right)',
-            image: 'https://i.imgur.com/VIOmjml.jpg',
-            description: '70 miles north of New York City two 20th century power plants stradle the western shore of the Hudson River.  These two plants - the Roseton ',
+            id: 'section 1',
+            title: 'The Danskammer Power Plant, New York State, 2019',
+            image: 'https://i.imgur.com/7AGvd6V.png',
+            description: 'text',
             location: {
-                center: [-73.96702, 41.64499],
-                zoom: 6.75,
-                pitch: 15.00,
-                bearing:10.40
+                center: [-74.35006, 41.37288],
+                zoom: 8.38,
+                pitch: 0.00,
+                bearing:0.00
             },
-            onChapterEnter: [
-                // {
-                //     layer: 'layer-name',
-                //     opacity: 1
-                // }
-            ],
+          
+    onChapterEnter: [
+      {
+          layer: 'danskammer-pt',
+          opacity: 1
+      },
+    ],
+    onChapterExit: [
+      {
+          layer: 'danskammer-pt',
+          opacity: 0.5
+      },
+    ]
+},
+
+          {
+            id: 'section 2',
+            title: 'Emissions Hotspot: the Roseton + Danskammer Plants',
+            image: 'https://i.imgur.com/ifNWZDv.png',
+            description: 'text, text, texttext, text, texttext, text, texttext, text, texttext, text, texttext, text, texttext, text, texttext, text, texttext, text, texttext, text, texttext, text, texttext, text, texttext, text, texttext, text, texttext, text, texttext, text, texttext, text, texttext, text, texttext, text, texttext, text, texttext, text, texttext, text, texttext, text, texttext, text, texttext, text, texttext, text, texttext, text, texttext, text, texttext, text, text',
+            location: {
+            	center: [-74.04912, 41.59372],
+				zoom: 10.93,
+				pitch: 0.00,
+				bearing: 0.00
+              
+            },
+    onChapterEnter: [
+	  {
+          layer: 'danskammer-pt',
+          opacity: 1
+      },
+
+    ],
             onChapterExit: [
-                // {
-                //     layer: 'layer-name',
-                //     opacity: 0
-                // }
-            ]
+	  {
+          layer: 'danskammer-pt',
+          opacity: 0.5
+      },
+    ]
         },
+
         {
-            id: 'other-identifier',
-            title: 'Second Title',
+            id: 'third',
+            title: 'Third Title',
             image: './path/to/image/source.png',
             description: 'Copy these sections to add to your story.',
             location: {
-                center: [-73.96960, 41.57377],
-                zoom: 15.21,
-                pitch: 36.00,
-                bearing: -74.40
+                center: [-73.99389, 41.57964],
+                zoom: 11.75,
+                pitch: 0.00,
+                bearing: -85.00
             },
             onChapterEnter: [],
             onChapterExit: []
